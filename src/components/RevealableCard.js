00f4c7e9eaceb50ddb78f9bd7c5923223a02f4e5
skareemoff <sherali.karimov@gmail.com>
2024-12-16 13:20:34 +0100

    working on better blur for RevealableCard

--- src/components/RevealableCard.js
+++ src/components/RevealableCard.js
@@ -25,10 +25,10 @@ const RevealableCard = (cardData) => {
                 cardStyle={cardData.cardStyle}
             />
             {isShowRevealButton ?
-            <View name="cover" style={[styles.cover, { height: calculateCardHeight(cardData), width: width * 0.9, marginHorizontal: width * 0.05 } ]} >
-                <Image source={require('../assets/images/qodmask.png')} style={{ width: '100%', height: '100%' }} />
-                <TouchableOpacity style={[{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center' }]} onPressOut={() => clickReveal()}>
-                  <Text style={styles.revealButton} source={require("../assets/images/button-play.png")} >Reveal </Text>
+            <View name="cover" style={[styles.cover, { height: calculateCardHeight(cardData) * 0.8, width: width * 0.85 } ]} >
+                <Image source={require('../assets/images/qodmask-small.png')} style={styles.blurImage} />
+                <TouchableOpacity style={styles.revealButton } onPressOut={() => clickReveal()}>
+                  <Text style={styles.revealButtonText} source={require("../assets/images/button-play.png")} >Reveal </Text>
                 </TouchableOpacity>
               </View>
             :
@@ -47,26 +47,40 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   cover: {
-    backgroundColor: 'white',
+    flex: 1,
+    backgroundColor: 'transparent',
     position: "absolute",
-    borderRadius: 20,
+    borderRadius: 30,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
     maxWidth: 400,
     zIndex: 1,
     top: 0,
     opacity: 1,
   },
+  blurImage: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: 50,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    borderRadius: 30,
+  },
   revealButton: {
-    textAlign: "center",
     justifyContent: 'center',
     borderRadius: 24,
-    padding: 12,
-    alignItems: 'center',
     shadowColor: '#000',
     shadowOpacity: 0.5,
     shadowOffset: {width: 2, height: 2 },
     width: 120,
     height: 60,
     backgroundColor: "white",
+  },
+  revealButtonText: {
+    textAlign: "center",
+    justifyContent: 'center',
     color: "black",
     fontFamily: "DM Sans",
     fontSize: 24,
