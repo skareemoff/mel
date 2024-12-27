c0bd09e5eaa73b04c00f62979ca3e02c2144de30
skareemoff <sherali.karimov@gmail.com>
2024-12-28 00:09:08 +0100

    preparing for push notifications

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { registerRootComponent } from 'expo';
-
+import { AppRegistry } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import { name as appName } from './app.json';
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
 registerRootComponent(App);
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Background message handled:', remoteMessage);
+});
+AppRegistry.registerComponent(appName, () => App);
