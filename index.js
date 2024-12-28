d410e040d37270bc773a30a30aace349749c43c1
skareemoff <sherali.karimov@gmail.com>
2024-12-28 21:53:25 +0100

    clearing out previous changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { registerRootComponent } from 'expo';
-import { AppRegistry } from 'react-native';
-import messaging from '@react-native-firebase/messaging';
-import { name as appName } from './app.json';
+// import { AppRegistry } from 'react-native';
+// import messaging from '@react-native-firebase/messaging';
+// import { name as appName } from './app.json';
 import App from './App';
 
 registerRootComponent(App);
 
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-    console.log('Background message handled:', remoteMessage);
-});
-AppRegistry.registerComponent(appName, () => App);
+// messaging().setBackgroundMessageHandler(async remoteMessage => {
+//     console.log('Background message handled:', remoteMessage);
+// });
+// AppRegistry.registerComponent(appName, () => App);
