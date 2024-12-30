afa9519bb967228decdc10975bbc8732095601ad
skareemoff <sherali.karimov@gmail.com>
2024-12-30 09:37:55 +0100

    Refactor Firebase integration and update dependencies for push notifications

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import { registerRootComponent } from 'expo';
-// import { AppRegistry } from 'react-native';
-// import messaging from '@react-native-firebase/messaging';
-// import { name as appName } from './app.json';
+import { AppRegistry } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 import App from './App';
+import { name as appName } from './app.json';
 
 registerRootComponent(App);
 
-// messaging().setBackgroundMessageHandler(async remoteMessage => {
-//     console.log('Background message handled:', remoteMessage);
-// });
-// AppRegistry.registerComponent(appName, () => App);
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Background message handled:', remoteMessage);
+});
+messaging().onMessage(remoteMessage => {
+    // Display the notification to the user
+    console.log('Foreground message:', remoteMessage);
+});
+messaging().onNotificationOpenedApp(remoteMessage => {
+    // Handle notification interaction when the app is in the foreground
+    console.log('App opened by notification while in foreground:', remoteMessage);
+});
+messaging().getInitialNotification().then(remoteMessage => {
+    // Handle notification interaction when the app is opened from a closed state
+    console.log('App opened by notification from closed state:', remoteMessage);
+});
+
+AppRegistry.registerComponent(appName, () => App);
