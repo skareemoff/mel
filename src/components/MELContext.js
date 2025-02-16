cbf70489f05f3fa0c61a8401c38aaf5d05192938
skareemoff <sherali.karimov@gmail.com>
2025-02-16 11:25:56 +0100

    indenting

--- src/components/MELContext.js
+++ src/components/MELContext.js
@@ -5,20 +5,20 @@ import { qodD } from "./QuestionOfTheDayData";
 const MELContext = createContext();
 
 const MELContextProvider = ({ children }) => {
-    const [dd] = useState(deckDataInstance);
-    const [favouriteState, setFavouriteState] = useState(0);
-    const [purchaseState, setPurchaseState] = useState(false);
-    const [questionOfTheDayState, setQuestionOfTheDayState] = useState(0);
+  const [dd] = useState(deckDataInstance);
+  const [favouriteState, setFavouriteState] = useState(0);
+  const [purchaseState, setPurchaseState] = useState(false);
+  const [questionOfTheDayState, setQuestionOfTheDayState] = useState(0);
 
   useEffect(() => {
     qodD.loadQoD(setQuestionOfTheDayState);
   }, []);
 
-    return (
-      <MELContext.Provider value={{dd, favouriteState, setFavouriteState, purchaseState, setPurchaseState, questionOfTheDayState, setQuestionOfTheDayState}}>
-        {children}
-      </MELContext.Provider>
-    );
-  };
+  return (
+    <MELContext.Provider value={{dd, favouriteState, setFavouriteState, purchaseState, setPurchaseState, questionOfTheDayState, setQuestionOfTheDayState}}>
+      {children}
+    </MELContext.Provider>
+  );
+};
 
   export { MELContext, MELContextProvider };
